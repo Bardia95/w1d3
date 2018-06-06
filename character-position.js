7adbe242c9dca334d8610a56c2d02eac436ce6b6
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2018-06-06 16:03:58 +0000

    fixed

--- character-position.js
+++ character-position.js
@@ -3,9 +3,9 @@ function characterPosition(string) {
   var array = string.toLowerCase().split("");
   for (var i = 0; i < array.length; i++) {
     if (!positions[array[i]] && array[i] != " ") {
-      positions[array[i]] = [array.indexOf(array[i])];
+      positions[array[i]] = [i];
     } else if (array[i] != " ") {
-      positions[array[i]].push(array.indexOf(array[i], i));
+      positions[array[i]].push(i);
     }
   };
   return positions
